f6a15f2232fa0868b33bc903e7fd636af7d1e7e3
Moisesunu <mofo1209@gmail.com>
2025-01-13 21:47:09 -0600

    Rename plugin and update metadata; add settings and utility functions for embedding code files

--- main.ts
+++ main.ts
@@ -1,85 +1,24 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { Plugin, MarkdownRenderer, TFile, MarkdownPostProcessorContext, MarkdownView, parseYaml, requestUrl} from 'obsidian';
+import { EmbedCodeFileSettings, EmbedCodeFileSettingTab, DEFAULT_SETTINGS} from "./settings";
+import { analyseSrcLines, extractSrcLines} from "./utils";
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
-}
-
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
-
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class EmbedCodeFile extends Plugin {
+	settings: EmbedCodeFileSettings;
 
 	async onload() {
 		await this.loadSettings();
+		this.addSettingTab(new EmbedCodeFileSettingTab(this.app, this));
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
+		this.registerMarkdownPostProcessor((element, context) => {
+			this.addTitle(element, context);
 		});
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
+		// live preview renderers
+		const supportedLanguages = this.settings.includedLanguages.split(",")
+		supportedLanguages.forEach(l => {
+			console.log(`registering renderer for ${l}`)
+			this.registerRenderer(l)
 		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-	}
-
-	onunload() {
-
 	}
 
 	async loadSettings() {
@@ -89,46 +28,147 @@ export default class MyPlugin extends Plugin {
 	async saveSettings() {
 		await this.saveData(this.settings);
 	}
-}
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
+	async registerRenderer(lang: string) {
+		this.registerMarkdownCodeBlockProcessor(`embed-${lang}`, async (meta, el, ctx) => {
+			let fullSrc = ""
+			let src = ""
+
+			let metaYaml: any
+			try {
+				metaYaml = parseYaml(meta)
+			} catch(e) {
+				await MarkdownRenderer.renderMarkdown("`ERROR: invalid embedding (invalid YAML)`", el, '', this)
+				return
+			}
+
+			let srcPath = metaYaml.PATH
+			if (!srcPath) {
+				await MarkdownRenderer.renderMarkdown("`ERROR: invalid source path`", el, '', this)
+				return
+			}
+
+			// Si la ruta comienza con "vault://", se está haciendo referencia a un archivo dentro de la bóveda
+			if (srcPath.startsWith("vault://")) {
+				// Eliminar el prefijo "vault://" de la ruta
+				srcPath = srcPath.replace(/^(vault:\/\/)/,'');
+
+				// Obtener el archivo de la bóveda utilizando la ruta proporcionada
+				const tFile = app.vault.getAbstractFileByPath(srcPath)
+
+				// Verificar si el archivo existe y es un archivo TFile (archivo de texto)
+				if (tFile instanceof TFile) {
+					// Leer el contenido del archivo
+					fullSrc = await app.vault.read(tFile)
+				} else {
+					// Mostrar un mensaje de error si el archivo no se puede leer
+					const errMsg = `\`ERROR: could't read file '${srcPath}'\``
+					await MarkdownRenderer.renderMarkdown(errMsg, el, '', this)
+					return
+				}
+			} else {
+				// Si la ruta no comienza con "vault://", se asume que es una URL remota
+				try {
+					// Intentar obtener el contenido del archivo desde la URL remota
+					let httpResp = await requestUrl({url: srcPath, method: "GET"})
+					fullSrc = httpResp.text
+				} catch(e) {
+					// Mostrar un mensaje de error si no se puede obtener el archivo desde la URL remota
+					const errMsg = `\`ERROR: could't fetch '${srcPath}'\``
+					await MarkdownRenderer.renderMarkdown(errMsg, el, '', this)
+					return
+				}
+			}
+
+			let srcLinesNum: number[] = []
+			const srcLinesNumString = metaYaml.LINES
+			if (srcLinesNumString) {
+				srcLinesNum = analyseSrcLines(srcLinesNumString)
+			}
+
+			if (srcLinesNum.length == 0) {
+				src = fullSrc
+			} else {
+				src = extractSrcLines(fullSrc, srcLinesNum)
+			}
+
+			let title = metaYaml.TITLE
+			if (!title) {
+				title = srcPath
+			}
+
+			// Renderizar el código en un bloque de código Markdown
+			await MarkdownRenderer.renderMarkdown('```' + lang + '\n' + src + '\n```', el, '', this)
+
+			// Añadir el título al bloque de código en la vista previa en vivo
+			this.addTitleLivePreview(el, title);
+		});
 	}
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
+	// Añadir un título a un bloque de código en la vista previa en vivo
+	addTitleLivePreview(el: HTMLElement, title: string) {
+		// Buscar el elemento de código dentro del bloque de código
+		const codeElm = el.querySelector('pre > code')
+		if (!codeElm) { return }
+		const pre = codeElm.parentElement as HTMLPreElement;
+		this.insertTitlePreElement(pre, title)
 	}
 
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
+	// Añadir un título a un bloque de código en la vista previa
+	addTitle(el: HTMLElement, context: MarkdownPostProcessorContext) {
+		// Buscar el elemento de código dentro del bloque de código
+		let codeElm = el.querySelector('pre > code')
+		if (!codeElm) {
+			return
+		}
+
+		const pre = codeElm.parentElement as HTMLPreElement;
+		const codeSection = context.getSectionInfo(pre)
+		if (!codeSection) {
+			return
+		}
+
+		const view = app.workspace.getActiveViewOfType(MarkdownView)
+		if (!view) {
+			return
+		}
+
+		// Obtener el número de línea donde comienza el bloque de código
+		const num = codeSection.lineStart
+		const codeBlockFirstLine = view.editor.getLine(num)
+
+		// Buscar el título en la primera línea del bloque de código
+		let matchTitle = codeBlockFirstLine.match(/TITLE:\s*"([^"]*)"/i)
+		if (matchTitle == null) {
+			return
+		}
+
+		const title = matchTitle[1]
+		if (title == "") {
+			return
+		}
+
+		// Insertar el título en el bloque de código
+		this.insertTitlePreElement(pre, title)
 	}
-}
 
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+	// Insertar el título en el elemento pre del bloque de código
+	insertTitlePreElement(pre: HTMLPreElement, title: string) {
+		// Eliminar cualquier título anterior
+		pre
+		.querySelectorAll(".obsidian-embed-code-file")
+		.forEach((x) => x.remove());
 
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
+		// Crear un nuevo elemento pre para el título
+		let titleElement = document.createElement("pre");
+		titleElement.appendText(title);
+		titleElement.className = "obsidian-embed-code-file";
 
-	display(): void {
-		const {containerEl} = this;
+		// Establecer los estilos del título
+		titleElement.style.color = this.settings.titleFontColor;
+		titleElement.style.backgroundColor = this.settings.titleBackgroundColor;
 
-		containerEl.empty();
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
+		// Añadir el título al bloque de código
+		pre.prepend(titleElement);
 	}
 }
---
+++ settings.ts
@@ -0,0 +1,62 @@
+import EmbedCodeFile from './main';
+
+import { PluginSettingTab, Setting, App } from 'obsidian';
+
+export interface EmbedCodeFileSettings {
+	includedLanguages: string;
+	titleBackgroundColor: string;
+	titleFontColor: string;
+}
+
+export const DEFAULT_SETTINGS: EmbedCodeFileSettings = {
+	includedLanguages: 'c,cs,cpp,java,python,go,ruby,javascript,js,typescript,ts,shell,sh,bash',
+	titleBackgroundColor: "#00000020",
+	titleFontColor: ""
+}
+
+export class EmbedCodeFileSettingTab extends PluginSettingTab {
+	plugin: EmbedCodeFile;
+
+	constructor(app: App, plugin: EmbedCodeFile) {
+		super(app, plugin);
+		this.plugin = plugin;
+	}
+
+	display(): void {
+		const {containerEl} = this;
+
+		containerEl.empty();
+		containerEl.createEl('h2', {text: 'Embed Code File Settings'});
+
+		new Setting(containerEl)
+			.setName('Included Languages')
+			.setDesc('Comma separated list of included languages.')
+			.addText(text => text
+				.setPlaceholder('Comma separated list')
+				.setValue(this.plugin.settings.includedLanguages)
+				.onChange(async (value) => {
+					this.plugin.settings.includedLanguages = value;
+					await this.plugin.saveSettings();
+				}));
+
+		new Setting(containerEl)
+			.setName("Font color of title")
+			.addText(text => text
+				.setPlaceholder('Enter a color')
+				.setValue(this.plugin.settings.titleFontColor)
+				.onChange(async (value) => {
+					this.plugin.settings.titleFontColor = value;
+					await this.plugin.saveSettings();
+				}));
+		  
+		new Setting(containerEl)
+			.setName('Background color of title')
+			.addText(text => text
+				.setPlaceholder('#00000020')
+				.setValue(this.plugin.settings.titleBackgroundColor)
+				.onChange(async (value) => {
+					this.plugin.settings.titleBackgroundColor = value;
+					await this.plugin.saveSettings();
+				}));
+	}
+}
